3a9f85f55a1b22996ebcdf00f53a1c4ab5a90a6a
Shidu <shidulab@gmail.com>
2026-09-24 20:47:52 +0200

    Prepare ComboCode J8.0.2: combocode/app.py

--- combocode/app.py
+++ combocode/app.py
@@ -14,7 +14,7 @@ def _prepare_windows_process() -> None:
         return
     try:
         import ctypes
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('ShiduLab.ComboCode.J8.0.1')
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('ShiduLab.ComboCode.J8.0.2')
     except Exception:
         pass
     try:
